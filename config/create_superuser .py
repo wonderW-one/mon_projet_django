ba43c3febaac3ce245aeb5a1242f5199928e35f9
wonder <iyizirewenny004@gmail.com>
2026-07-15 10:27:48 +0200

    wonder

---
+++ config/create_superuser .py
@@ -0,0 +1,18 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+username = "admin_prod"
+email = "admin@monprojet.com"
+password = os.environ.get('ADMIN_PASSWORD', 'ChangeMoiTemporairement123!')
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f"Superutilisateur {username} créé avec succès !")
+else:
+    print("Le superutilisateur existe déjà.")
